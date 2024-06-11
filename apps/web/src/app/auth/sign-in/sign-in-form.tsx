04471835208c786df42271048a7c996664256ab6
Pedro Brasca <pedropaulobrasca@gmail.com>
2024-06-10 21:17:25 -0300

    feat: estados de erro do formulário

--- apps/web/src/app/auth/sign-in/sign-in-form.tsx
+++ apps/web/src/app/auth/sign-in/sign-in-form.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import { Loader2 } from 'lucide-react'
+import { AlertTriangle, Loader2 } from 'lucide-react'
 import Link from 'next/link'
 import { useActionState } from 'react'
 
 import GithubIcon from '@/assets/github-icon'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -15,29 +16,49 @@ import { signInWithEmailAndPassword } from './actions'
 export function SignInForm() {
   const [state, formAction, isPending] = useActionState(
     signInWithEmailAndPassword,
-    null,
+    { success: false, message: null, errors: null },
   )
 
   return (
     <form action={formAction} className="space-y-4">
-      <h1>{state}</h1>
+      {state.success === false && state.message && (
+        <Alert variant="destructive">
+          <AlertTriangle className="size-4" />
+          <AlertTitle>Sign in failed!</AlertTitle>
+          <AlertDescription>
+            <p>{state.message}</p>
+          </AlertDescription>
+        </Alert>
+      )}
 
       <div className="space-y-1">
         <Label htmlFor="email">E-mail</Label>
         <Input name="email" type="email" id="email" />
+
+        {state.errors?.email && (
+          <p className="text-xs font-medium text-red-500 dark:text-red-400">
+            {state.errors.email[0]}
+          </p>
+        )}
       </div>
 
       <div className="space-y-1">
         <Label htmlFor="password">Password</Label>
         <Input name="password" type="password" id="password" />
 
-        <Link
-          href="/auth/forgot-password"
-          className="text-sm font-medium text-foreground hover:underline"
-        >
-          Forgot your password?
-        </Link>
+        {state.errors?.password && (
+          <p className="text-xs font-medium text-red-500 dark:text-red-400">
+            {state.errors.password[0]}
+          </p>
+        )}
       </div>
+
+      <Link
+        href="/auth/forgot-password"
+        className="text-sm font-medium text-foreground hover:underline"
+      >
+        Forgot your password?
+      </Link>
 
       <Button type="submit" className="w-full" disabled={isPending}>
         {isPending ? (
