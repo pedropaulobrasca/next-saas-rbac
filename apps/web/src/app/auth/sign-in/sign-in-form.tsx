2beb5c4135b59afc1889ca0ce80fb173a14c7fcd
Pedro Brasca <pedropaulobrasca@gmail.com>
2024-06-10 21:22:46 -0300

    feat: evitando reset do formulario

--- apps/web/src/app/auth/sign-in/sign-in-form.tsx
+++ apps/web/src/app/auth/sign-in/sign-in-form.tsx
@@ -2,7 +2,7 @@
 
 import { AlertTriangle, Loader2 } from 'lucide-react'
 import Link from 'next/link'
-import { useActionState } from 'react'
+import { FormEvent, useState, useTransition } from 'react'
 
 import GithubIcon from '@/assets/github-icon'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
@@ -14,19 +14,43 @@ import { Separator } from '@/components/ui/separator'
 import { signInWithEmailAndPassword } from './actions'
 
 export function SignInForm() {
-  const [state, formAction, isPending] = useActionState(
-    signInWithEmailAndPassword,
-    { success: false, message: null, errors: null },
-  )
+  // const [{ errors, message, success }, formAction, isPending] = useActionState(
+  //   signInWithEmailAndPassword,
+  //   { success: false, message: null, errors: null },
+  // )
+
+  const [isPending, startTransition] = useTransition()
+  const [{ success, message, errors }, setFormState] = useState<{
+    success: boolean
+    message: string | null
+    errors: Record<string, string[]> | null
+  }>({
+    success: false,
+    message: null,
+    errors: null,
+  })
+
+  async function handleSignIn(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const form = event.currentTarget
+    const data = new FormData(form)
+
+    startTransition(async () => {
+      const state = await signInWithEmailAndPassword(data)
+
+      setFormState(state)
+    })
+  }
 
   return (
-    <form action={formAction} className="space-y-4">
-      {state.success === false && state.message && (
+    <form onSubmit={handleSignIn} className="space-y-4">
+      {success === false && message && (
         <Alert variant="destructive">
           <AlertTriangle className="size-4" />
           <AlertTitle>Sign in failed!</AlertTitle>
           <AlertDescription>
-            <p>{state.message}</p>
+            <p>{message}</p>
           </AlertDescription>
         </Alert>
       )}
@@ -35,9 +59,9 @@ export function SignInForm() {
         <Label htmlFor="email">E-mail</Label>
         <Input name="email" type="email" id="email" />
 
-        {state.errors?.email && (
+        {errors?.email && (
           <p className="text-xs font-medium text-red-500 dark:text-red-400">
-            {state.errors.email[0]}
+            {errors.email[0]}
           </p>
         )}
       </div>
@@ -46,21 +70,21 @@ export function SignInForm() {
         <Label htmlFor="password">Password</Label>
         <Input name="password" type="password" id="password" />
 
-        {state.errors?.password && (
+        {errors?.password && (
           <p className="text-xs font-medium text-red-500 dark:text-red-400">
-            {state.errors.password[0]}
+            {errors.password[0]}
           </p>
         )}
       </div>
 
       <Link
         href="/auth/forgot-password"
-        className="text-sm font-medium text-foreground hover:underline"
+        className="text-xs font-medium text-foreground hover:underline"
       >
         Forgot your password?
       </Link>
 
-      <Button type="submit" className="w-full" disabled={isPending}>
+      <Button className="w-full" type="submit" disabled={isPending}>
         {isPending ? (
           <Loader2 className="size-4 animate-spin" />
         ) : (
@@ -68,11 +92,15 @@ export function SignInForm() {
         )}
       </Button>
 
+      <Button className="w-full" variant="link" size="sm" asChild>
+        <Link href="/auth/sign-up">Create new account</Link>
+      </Button>
+
       <Separator />
 
       <Button type="submit" className="w-full" variant="outline">
         <GithubIcon className="mr-2 size-4 dark:invert" />
-        Sign in with Github
+        Sign in with GitHub
       </Button>
     </form>
   )
