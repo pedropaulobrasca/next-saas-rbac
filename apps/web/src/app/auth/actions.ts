f0eaac2c112bd534c01bbf8fafb7518f306ea2dd
Pedro Brasca <pedropaulobrasca@gmail.com>
2024-06-11 21:13:10 -0300

    feat: autenticacao com github

---
+++ apps/web/src/app/auth/actions.ts
@@ -0,0 +1,16 @@
+'use server'
+
+import { redirect } from 'next/navigation'
+
+export async function signInWithGithub() {
+  const githubSignInURL = new URL('login/oauth/authorize', 'https://github.com')
+
+  githubSignInURL.searchParams.set('client_id', '0ae9890b19bc2133ed33')
+  githubSignInURL.searchParams.set(
+    'redirect_uri',
+    'http://localhost:3000/api/auth/callback',
+  )
+  githubSignInURL.searchParams.set('scope', 'user')
+
+  redirect(githubSignInURL.toString())
+}
