04ad91491ca6f58daa787e7de0d76d5482de7c8b
Pedro Brasca <pedropaulobrasca@gmail.com>
2024-06-16 21:35:35 -0300

    fix: ajuste no header

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -1,21 +1,16 @@
 import { redirect } from 'next/navigation'
 
 import { isAuthenticated } from '@/auth/auth'
-import Header from '@/components/header'
 
-export default function AuthLayout({
+export default function AppLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
+  teste: React.ReactNode
 }>) {
   if (!isAuthenticated()) {
     redirect('/auth/sign-in')
   }
 
-  return (
-    <div className="space-y-4 py-4">
-      <Header />
-      <main className="mx-auto w-full max-w-[1200px]">{children}</main>
-    </div>
-  )
+  return <>{children}</>
 }
