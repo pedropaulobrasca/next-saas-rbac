61692bbeef04ea301103ff799687d9689fbb3b11
Pedro Brasca <pedro.brasca@gmail.com>
2024-08-03 21:10:02 -0300

    feat: navegacao do app por abas

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -13,7 +13,7 @@ export default async function Header() {
   const permissions = await ability()
 
   return (
-    <div className="mx-auto flex max-w-[1200px] items-center justify-between border-b pb-2">
+    <div className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
         <Link href="/">
           <Rocket className="size-6" />
