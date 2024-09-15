d2caf6d5b7f2e65e1f62ca04d78257c050a0922c
Pedro Brasca <pedropaulobrasca@gmail.com>
2024-09-15 20:22:27 -0300

    🐛 fix: problemas no eslint

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -1,12 +1,13 @@
 'use client'
 
-import { queryClient } from "@/lib/react-query";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ThemeProvider } from "next-themes";
-import { ReactNode } from "react";
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ThemeProvider } from 'next-themes'
+import { ReactNode } from 'react'
 
-export function Providers({children}: {children: ReactNode}){
-  return(
+import { queryClient } from '@/lib/react-query'
+
+export function Providers({ children }: { children: ReactNode }) {
+  return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider
         attribute="class"
